ec7188b17f18e58dbfb05e5f23c8db8338dc4979
Mihaela-py <57058451+Mihaela-py@users.noreply.github.com>
2019-11-05 09:20:01 +0200

    Add files via upload

---
+++ Miha/HomeworkExtra.py
@@ -0,0 +1,38 @@
+"""print("ooooooooooooooooo")
+print("ooooooooooooooooo")
+print("ooooooooooooooooo")
+print("oooo")
+print("oooo")
+print("oooo")
+print("ooooooooooooooooo")
+print("ooooooooooooooooo")
+print("ooooooooooooooooo")
+print("             oooo")
+print("             oooo")
+print("             oooo")
+print("ooooooooooooooooo")
+print("ooooooooooooooooo")
+print("ooooooooooooooooo")
+"""
+
+
+"""import re
+parola=input("Introduceti parola:")
+x=True
+while x:
+    if (len(parola)<7):
+        break
+    elif not re.search("[a-z]",parola):
+        break
+    elif not re.search("[0-7]",parola):
+        break
+    elif not re.search("[A-Z]",parola):
+        break
+    elif re.search("\s",parola):
+        break
+    else:
+        print("Parola este valida")
+        x=False
+        break
+if x:
+    print("Parola nu este valida! ")"""
